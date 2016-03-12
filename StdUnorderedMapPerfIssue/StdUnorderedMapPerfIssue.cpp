36095f15abdf6083b1d2e4f967b5d4c17510fff1
hershi <hershi@gmail.com>
2016-03-11 21:40:04 -0800

    Initial blank version of the StdUnorderedMapPerfIssues project

---
+++ StdUnorderedMapPerfIssue/StdUnorderedMapPerfIssue.cpp
@@ -0,0 +1,13 @@
+// This sample project demonstrates the performance issues arising when using std::unordered_map with
+// hash values that share the same lower-order bits
+
+#include <tchar.h>
+#include <chrono>
+#include <unordered_map>
+#include <boost/unordered_map.hpp>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
